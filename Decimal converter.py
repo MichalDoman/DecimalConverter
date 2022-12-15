013b514efb8c9d0f4a2c9b4135c7ed431ff43672
michaldoman <mjd.domanski@gmail.com>
2022-12-15 22:27:13 +0100

    version 1.1 (works on most variants)

--- Decimal converter.py
+++ Decimal converter.py
@@ -8,7 +8,7 @@ def main():
         if not integer_test(input_value):
             print('This is not a number! Try again: ')
             continue
-        decimal_to_binary(int(input_value), 2)
+        print(decimal_to_binary(int(input_value), 2))
 
 
 def integer_test(number):
@@ -24,17 +24,45 @@ def integer_test(number):
         return False
 
 
+def conversion_test(number, outcome, numeral_system):
+    total = 0
+    for i in range(1, len(outcome) + 1):
+        if outcome[int(i) - 1] == '1':
+            total += (numeral_system ** (int(i) - 1))  # Works only for binary
+    if total == number:
+        return True
+    else:
+        return False
+
+
 def decimal_to_binary(number, numeral_system):
     i = 0
     total = 0
-    binary = ''
+    binary = []
+
     while (numeral_system ** i) <= number:
         i += 1
-    binary += '1'
-    total += (numeral_system ** (i - 1))
-    print(total)
-    print(binary)
 
+    length = i
+    for digit in range(length - 1):
+        binary.append('0')
+    binary.append('1')
+    total += (numeral_system ** (i - 1))
+    diff = number - total
+    binary_str = ''.join(binary)
+    while not conversion_test(number, binary_str, numeral_system):
+        if diff == 0:
+            return binary_str
+        else:
+            i = 0
+            while (numeral_system ** i) <= diff:
+                i += 1
+            total += (numeral_system ** (i - 1))
+            print(total)
+            print(diff)
+            binary[i - 1] = '1'
+            binary_str = ''.join(binary)
+    return binary_str
 
 if __name__ == '__main__':
     main()
