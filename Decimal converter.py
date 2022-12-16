39a7c0026617c114aad7fb15cb1d4673ed2b0c36
michaldoman <mjd.domanski@gmail.com>
2022-12-16 19:13:18 +0100

    version 1.3 (multi-sys, proper display)

--- Decimal converter.py
+++ Decimal converter.py
@@ -1,3 +1,6 @@
+from math import floor
+
+
 def main():
     print('-----------------------------------')
     print(' Welcome to the Decimal Converter!')
@@ -9,9 +12,15 @@ def main():
             print('This is not a valid entry! Try again: ')
             print()
             continue
-        # print(decimal_to_binary(int(input_value), 2))
+        # print(convert_decimals(int(input_value), 3))
         print()
-        print(conversion_test(7, '12', 3))
+        proper_display('0')
+        proper_display('01')
+        proper_display('012')
+        proper_display('0123')
+        proper_display('01234')
+        proper_display('012345')
+        proper_display('0123456')
 
 
 def input_test(number):
@@ -27,7 +36,7 @@ def input_test(number):
         return False
 
 
-def conversion_test(number, outcome, numeral_system):
+def conversion_test(number, outcome, numeral_system):  # takes only reversed numbers
     total = 0
     for i in range(1, len(outcome) + 1):
         if outcome[int(i) - 1] != '0':
@@ -38,31 +47,55 @@ def conversion_test(number, outcome, numeral_system):
         return False
 
 
-def decimal_to_binary(number, numeral_system):
+def convert_decimals(number, numeral_system):
     total = 0
-    binary = []
+    new_digits = []
 
     i = 0
     while (numeral_system ** i) <= number:
         i += 1
 
-    length = i
-    for digit in range(length - 1):
-        binary.append('0')
-    binary.append('1')
-    total += (numeral_system ** (i - 1))
-    diff = number - total
-    binary_str = ''.join(binary)
+    number_length = i
+    for digit in range(number_length - 1):
+        new_digits.append('0')
 
-    while not conversion_test(number, binary_str, numeral_system):
+    digit = str(floor(number / (numeral_system ** (i - 1))))
+    new_digits.append(digit)
+    total += (numeral_system ** (i - 1)) * int(digit)
+    diff = number - total
+    new_digits_str = ''.join(new_digits)
+
+    while not conversion_test(number, new_digits_str, numeral_system):
         i = 0
         while (numeral_system ** i) <= diff:
             i += 1
-        total += (numeral_system ** (i - 1))
+        digit = str(floor(diff / (numeral_system ** (i - 1))))
+        total += (numeral_system ** (i - 1)) * int(digit)
         diff = number - total
-        binary[i - 1] = '1'
-        binary_str = ''.join(binary)
-    return binary_str[::-1]
+
+        new_digits[i - 1] = digit
+        new_digits_str = ''.join(new_digits)
+    return new_digits_str
+
+
+def proper_display(number_str):
+    new_str = ''
+    str_part = ''
+    temp = ''
+
+    for digit in number_str:
+        str_part += digit
+        temp += digit
+        if len(str_part) % 3 == 0:
+            temp = ''
+            str_part += ' '
+            new_str += str_part
+            str_part = ''
+    new_str += temp
+    new_str = new_str[::-1].strip()
+
+    return new_str
+
 
 if __name__ == '__main__':
     main()
